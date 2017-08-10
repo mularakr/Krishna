fea7b2af13de3eab92775f888ae72ec19c08319c
MulaRakesh Reddy <GS-1410@GSLAB.COM>
2017-08-09 19:55:35 -0500

    ew proj:

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'about-comp',
+    templateUrl:'./about.html'
+})
+export class AboutComponent{
+
+}
---
+++ src/app/components/navigation/navigation-component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector:'nav-comp',
+    templateUrl:'./navbar.html'
+})
+export class NavigationComponent{
+
+}
